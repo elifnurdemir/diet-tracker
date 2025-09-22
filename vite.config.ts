8f4acb15cd8fecef5b2533b6008e077f42e54166
elif <elifxnur118@gmail.com>
2025-09-22 08:33:24 +0300

    D

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/n", // repo adını yaz
+  base: "/diet-tracker/", // burayı repo adına göre ayarladık
 });
